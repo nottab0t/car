5f6bf5b760e5c94079246f10c4f7f10c5f1b0536
nottab0t <thisisnottabot@gmail.com>
2025-10-27 02:17:13 -0600

    smg + animations
    + new smg model
    + new hold animations for smg
    + item class for pickup and storage of ground items
    + switched to jolt physics
    + cleaned up physics layer names
    
    ! started making "item" class with smg, to be expanded and refactored as new items are added
    ! need to clean up player animation code and possibly the animation tree as well

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,4 +1,5 @@
 extends CharacterBody3D
+class_name Player
 
 const JUMP_VELOCITY = 5.0
 const LERP_VAL = .15
@@ -9,15 +10,44 @@ const LERP_VAL = .15
 @export var sens := 0.005
 @export var controller_sens := 1.5
 
+# children
 @onready var main : Main = self.get_parent()
 @onready var cam := $CamOrigin/SpringArm3D/PlayerCamera
 @onready var cam_arm := $CamOrigin/SpringArm3D
 @onready var pivot := $CamOrigin
-@onready var armature := $Armature
-@onready var anim_tree := $AnimationTree
+@onready var armature : Node3D = $Armature
+@onready var anim_tree : AnimationTree = $AnimationTree
+@onready var debug_label : Label3D = $DebugLabel
+@onready var item_trigger : Area3D = $ItemTrigger
+@onready var skeleton : Skeleton3D = $Armature/Skeleton3D
+
+var main_hand_pos : Vector3
+var main_hand_rot : Basis
+var main_hand_offset : float
+var off_hand_pos : Vector3
+var off_hand_rot : Basis
+
+var held_anim := "none"
+var held_anim_run := "none"
 
 func get_cam() -> Camera3D:
 	return cam
+
+func hold_item_anim(item: String) -> void:
+	anim_tree.set("parameters/Arm Blend/blend_amount", 1)
+	held_anim = "parameters/" + item + " Hold Blend/blend_amount"
+	held_anim_run = "parameters/" + item + " Hold Run Blend/blend_amount"
+	anim_tree.set(held_anim, 1)
+
+func drop_item_anim() -> void:
+	anim_tree.set(held_anim, 0)
+	anim_tree.set(held_anim_run, 0)
+	held_anim = "none"
+	held_anim_run = "none"
+	
+
+func _body_exited(body: Node3D):
+	body.label.visible = false
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -30,6 +60,11 @@ func _unhandled_input(event: InputEvent) -> void:
 		cam_arm.rotation.x = clamp(cam_arm.rotation.x, deg_to_rad(-75), deg_to_rad(20))
 
 func _physics_process(delta: float) -> void:
+	for body in item_trigger.get_overlapping_bodies():
+		if body is Item:
+			body.link_item(item_trigger)
+	
+	# hide/show based on control mode
 	if main.control_mode == "car":
 		self.hide()
 		$CollisionShape3D.disabled = true
@@ -44,26 +79,31 @@ func _physics_process(delta: float) -> void:
 	if not main.control_mode == "player": return
 	# camera control
 	if Input.is_action_pressed("look_right"):
-		rotation.y -= controller_sens * delta
+		pivot.rotation.y -= controller_sens * delta
 	if Input.is_action_pressed("look_left"):
-		rotation.y += controller_sens * delta
+		pivot.rotation.y += controller_sens * delta
 	if Input.is_action_pressed("look_up"):
-		pivot.rotation.x += controller_sens * delta
-		pivot.rotation.x = clamp(pivot.rotation.x, deg_to_rad(-90), deg_to_rad(10))
+		cam_arm.rotation.x += controller_sens * delta
+		cam_arm.rotation.x = clamp(pivot.rotation.x, deg_to_rad(-90), deg_to_rad(10))
 	if Input.is_action_pressed("look_down"):
-		pivot.rotation.x -= controller_sens * delta
-		pivot.rotation.x = clamp(pivot.rotation.x, deg_to_rad(-90), deg_to_rad(10))
+		cam_arm.rotation.x -= controller_sens * delta
+		cam_arm.rotation.x = clamp(pivot.rotation.x, deg_to_rad(-90), deg_to_rad(10))
 	
 	# sprint
 	if Input.is_action_pressed("run"): 
 		speed = run_speed
-		anim_tree.set("parameters/Run Blend/blend_amount", velocity.length() / run_speed)
-		anim_tree.set("parameters/Walk Blend/blend_amount", 0)
+		if not held_anim_run == "none": 
+			anim_tree.set(held_anim, 0)
+			anim_tree.set(held_anim_run, 1)
 	else: 
 		speed = 5.0
-		anim_tree.set("parameters/Walk Blend/blend_amount", velocity.length() / run_speed)
-		anim_tree.set("parameters/Run Blend/blend_amount", 0)
-		
+		if not held_anim_run == "none": 
+			anim_tree.set(held_anim, 1)
+			anim_tree.set(held_anim_run, 0)
+	
+	# walk & run animations
+	anim_tree.set("parameters/Walk Blend/blend_amount", velocity.length() / 5)
+	anim_tree.set("parameters/Run Blend/blend_amount", clampf((velocity.length() / 5) - 1, 0, 1))
 	
 	# zoom
 	if Input.is_action_pressed("zoom"): 
@@ -77,8 +117,24 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# jump/fall animation
+	if not is_on_floor():
+		anim_tree.set("parameters/Walk Blend/blend_amount", 0)
+		anim_tree.set("parameters/Run Blend/blend_amount", 0)
+		if not held_anim_run == "none": 
+			anim_tree.set(held_anim, 0)
+			anim_tree.set(held_anim_run, 1)
+		
+		if velocity.y > 0:
+			anim_tree.set("parameters/Jump Scale/scale", 0.5)
+			anim_tree.set("parameters/Jump Blend/blend_amount", 1)
+		elif velocity.y < 0:
+			anim_tree.set("parameters/Jump Scale/scale", -0.25)
+			anim_tree.set("parameters/Jump Blend/blend_amount", clampf(-velocity.y * 0.2, 1, 0.8))
+	else:
+		anim_tree.set("parameters/Jump Blend/blend_amount", 0)
+
+	# Get the input direction and handle the movement/deceleration
 	var input_dir := Input.get_vector("walk_left", "walk_right", "walk_forward", "walk_backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	direction = direction.rotated(Vector3.UP, pivot.rotation.y)
@@ -89,7 +145,13 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = lerp(velocity.x, 0.0, LERP_VAL)
 		velocity.z = lerp(velocity.z, 0.0, LERP_VAL)
-
-	anim_tree.set("parameters/Run Blend/blend_amount", velocity.length() / run_speed)
+	
+	# update hand positions
+	main_hand_pos = skeleton.get_bone_global_pose(10).origin
+	main_hand_rot = skeleton.get_bone_global_pose(10).basis
+	main_hand_offset = skeleton.get_bone_global_pose(10).origin.x
+	#off_hand_pos = global_position + skeleton.get_bone_global_pose(4).origin
+	#off_hand_rot = basis.z + skeleton.get_bone_global_pose(4).basis.z
+	
 
 	move_and_slide()
---
+++ Scripts/smg.gd
@@ -0,0 +1,83 @@
+extends RigidBody3D
+class_name Item
+
+# children
+@onready var label := $Label3D
+@onready var main_grip := $MainGripPoint
+@onready var collision := $CollisionShape3D
+@onready var model := $model 
+
+var signal_connected := false
+var near_player := false
+var player : Player
+var main : Main
+var equipped := false
+var grip_offset : Vector3
+var gripped_rotation := Vector3(deg_to_rad(-90), 0, deg_to_rad(180))
+
+
+# called by player when they are nearby
+func link_item(area: Area3D) -> void:
+	label.visible = true
+	near_player = true
+	
+	# connect signal if this is the first interaction with player
+	if signal_connected: return
+	area.body_exited.connect(_left_player_area)
+	player = area.get_parent()
+	signal_connected = true
+
+func _ready() -> void:
+	grip_offset = main_grip.position
+	main = get_parent()
+	
+
+# called via signal when player is no longer nearby
+func _left_player_area(_body: Node3D):
+	label.visible = false
+	near_player = false
+
+func _physics_process(_delta: float) -> void:
+	# keep label from rotating with item
+	label.global_position = self.global_position + Vector3.UP * 0.75
+	
+	if equipped:
+		# match position and basis to player main hand
+		position = player.main_hand_pos
+		basis = player.main_hand_rot
+		
+		# drop item only if equipped
+		if Input.is_action_pressed("drop_item"):
+			equipped = false
+			collision.disabled = false
+			# adjust model back to normal
+			global_position = model.global_position
+			global_rotation = model.global_rotation
+			model.rotation = Vector3.ZERO
+			model.position = Vector3.ZERO
+			self.linear_velocity = Vector3.ZERO
+			player.drop_item_anim()
+			# reparent back to main
+			self.reparent(main)
+		
+		# stop here if equipped to prevent ground checks
+		return
+	
+	# prevent ground checks if not near player
+	if not near_player: return
+	# player picks up item into storage
+	if Input.is_action_pressed("pickup_item"):
+		# update player inventory somehow
+		queue_free()
+	
+	# player equips item from ground
+	if Input.is_action_pressed("equip_item"):
+		equipped = true
+		collision.disabled = true
+		# adjust model to match hand
+		model.position = grip_offset
+		model.rotation = gripped_rotation
+		# reparent to make position/rotation easy
+		self.reparent(player.armature)
+		player.hold_item_anim("Smg")
+		
